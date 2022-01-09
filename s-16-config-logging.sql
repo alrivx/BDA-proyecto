5e8eba52766ed35b2001b49b2ae264916709cb13
alrivx <alexrnagano@gmail.com>
2022-01-08 22:15:17 -0600

    Script a ejecutar tras el primer respaldo

--- s-16-config-logging.sql
+++ s-16-config-logging.sql
@@ -1,3 +1,7 @@
-# @Autor: Héctor Eduardo Cabrera Beltrán & Alejandro Rivera Nagano
-# @Fecha: 6 de enero de 2022
-# @Descripción: 
+-- @Autor: Héctor Eduardo Cabrera Beltrán & Alejandro Rivera Nagano
+-- @Fecha: 6 de enero de 2022
+-- @Descripción: Configurar tablespaces para que generen REDO logs terminada la 
+-- carga inicial
+alter tablespace contenido_tbs logging;
+alter tablespace indexes_tbs logging;
+alter tablespace usuarios_tbs logging;
