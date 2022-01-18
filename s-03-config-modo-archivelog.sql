c79138d1bf29cab9e66721568a3717b930c9043f
Hector Cabrera <hecabe42@gmail.com>
2022-01-18 00:00:00 -0600

    scope=spfile para log_archive_format

--- s-03-config-modo-archivelog.sql
+++ s-03-config-modo-archivelog.sql
@@ -6,7 +6,7 @@
 create pfile='$ORACLE_BASE/oradata/CARIPROY/respaldo-pfile.txt' from spfile;
 !mkdir -p $ORACLE_BASE/oradata/CARIPROY/disk_4/archive
 alter system set log_archive_max_processes=6 scope=both;
-alter system set log_archive_format = 'arch_cariproy_%t_%s_%r_%d.arc' scope=both;
+alter system set log_archive_format = 'arch_cariproy_%t_%s_%r_%d.arc' scope=spfile;
 alter system set log_archive_trace=10 scope=both;
 alter system set log_archive_dest_1='LOCATION=/u01/app/oracle/oradata/CARIPROY/disk_4/archive MANDATORY' scope=both;
 alter system set log_archive_min_succeed_dest=1 scope=both;
